94c2a6646ea6a7fc26106868aee1735ab3c8ecae
ChamilaGayan <108798892+ChamilaGayan@users.noreply.github.com>
2022-08-04 12:18:25 +0530

    del mig

---
+++ database/2022_08_04_062821_image.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Image extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('image')->default('user.png')->after('password')->nullable();
+            $table->string('colours')->default('black')->after('image')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
